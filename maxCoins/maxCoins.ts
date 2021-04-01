365274cffad80b25fc34aca38f86fabcdd6593fb
AvraamMavridis <avraammauridis@gmail.com>
2021-04-01 14:57:22 +0200

    Perf: Slight improve on maxCoins

--- maxCoins/maxCoins.ts
+++ maxCoins/maxCoins.ts
@@ -1,14 +1,9 @@
-export default function maxCoins(piles: number[]): number {
+function maxCoins(piles: number[]): number {
   piles.sort((a, b) => a - b);
   const len = piles.length / 3;
 
   return piles.reduce((prev, next, index) => {
-    if(index < len) return prev + 0;
-
-    if ((index - len) % 2 === 0) {
-      return prev + next;
-    } else {
-      return prev;
-    }
+    if (index < len || (index - len) % 2 !== 0) return prev;
+    return prev + next;
   }, 0);
 }
