d565c245fbc81d641fbaa0ef0c0ccb6c42455b72
AvraamMavridis <avraammauridis@gmail.com>
2021-04-07 16:57:56 +0200

    Chore: Add Readme for reducing dishes

--- reducingDishes/reducingDishes.spec.ts
+++ reducingDishes/reducingDishes.spec.ts
@@ -4,7 +4,6 @@ describe('Reducing Dishes', () => {
   it('Returns the maximum sum of Like-time coefficient', () => {
     expect(reducingDishes([-1, -8, 0, 5, -9])).toEqual(14);
     expect(reducingDishes([4, 3, 2])).toEqual(20);
-    expect(reducingDishes([4, 3, 2])).toEqual(20);
     expect(reducingDishes([-1, -4, -5])).toEqual(0);
     expect(reducingDishes([-2, 5, -1, 0, 3, -3])).toEqual(35);
   });
